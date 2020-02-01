c8b2f9ec7889336956409eafd58b168ecca923ef
dyesteves09 <besteves.09@gmail.com>
2020-02-02 03:09:10 +0800

    Change message

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   return res.status(200).send({
-    message: 'I AM FLEXIBLE CHANGES'
+    message: 'I AM FLEXIBLE Superman'
   })
 });
 
