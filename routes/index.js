97503c1fa2117016acc9ea466be39d6eab3c4563
dyesteves09 <besteves.09@gmail.com>
2020-02-02 03:07:43 +0800

    First

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  return res.status(200).send({
+    message: 'I AM FLEXIBLE CHANGES'
+  })
+});
+
+module.exports = router;
